231a0ab485a0261c01b04fe644c512b7d97cf7e6
Huang Jie <root@linuxsand.info>
2016-09-05 09:49:32 +0800

    photo-resize-and-zip.py: do not overwrite images

--- resize_photo/photo-resize-and-zip.py
+++ resize_photo/photo-resize-and-zip.py
@@ -1,9 +1,13 @@
-import os, sys, random, string, shutil
+import os, random, string
 from PIL import Image # Pillow
+import shutil
+import sys
 
-def new_dir_name(length):
+QUALITY = 95
+
+def new_dir_name(l):
     s = string.ascii_letters + string.digits
-    return ''.join(random.sample(s, length))
+    return ''.join(random.sample(s, l))
 
 def do_zip(filename):
     shutil.make_archive(filename, 'zip', root_dir='.', base_dir=filename)
@@ -13,39 +17,37 @@ def main(size, length, img_path=None, mkdir=False):
     if mkdir:
         dir_ = new_dir_name(length)
         os.mkdir(dir_)
-        raw_input('put images in, then press ENTER.')
+        raw_input('put images in, then press <ENTER>')
         os.chdir(dir_)
         for img in os.listdir('.'):
             print 'processing %s...' % img
             im = Image.open(img)
             im.thumbnail(size, Image.ANTIALIAS)
-            im.save(img, quality=95)
-        raw_input('press ENTER to zip, press CTRL-C to exit.')
+            im.save('c_'+img, quality=QUALITY)
+            im.close()
+            os.remove(img)
+        ans = raw_input('press <ENTER> to zip, press `n` to exit.')
+        if ans.lower().strip() == 'n':
+            return
         os.chdir('..')
         do_zip(dir_)
     else:
         print 'processing %s...' % img_path
         im = Image.open(img_path)
         im.thumbnail(size, Image.ANTIALIAS)
-        im.save(img_path, quality=95)
+        par, img = os.path.split(img_path)
+        final_img = os.path.join(par, 'c_'+img)
+        im.save(final_img, quality=QUALITY)
+        im.close()
+        os.remove(img_path)
 
 if __name__ == '__main__':
     try:
-        length = int(sys.argv[1].strip())
+        l = int(sys.argv[1].strip())
         img_path = sys.argv[2]
-        main(
-                (length, length),
-                8,
-                img_path,
-                False
-            )
+        main((l, l), 8, img_path, False)
     except IndexError:
-        _length = raw_input('input min length that you want: ')
-        length = int(_length.strip())
-        main(
-                (length, length),
-                8,
-                None,
-                True
-            )
+        l = raw_input('input min length that you want: ')
+        l = int(l.strip())
+        main((l, l), 8, None, True)
        
