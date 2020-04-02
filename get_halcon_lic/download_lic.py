e7119ef7c1b2ee8c019e46b6bc7a51887493668f
Huang Jie <jie.huang@aqrose.com>
2020-04-02 11:00:23 +0800

    default download to desktop, user can specify where to store; add readme

--- get_halcon_lic/download_lic.py
+++ get_halcon_lic/download_lic.py
@@ -1,35 +1,40 @@
 # wrote this script for my friend LT
 import os
 
-base_url = "https://www.51halcon.com/down/license/"
+base_url = 'https://www.51halcon.com/down/license/'
 
-def py2_read_url(url, dir_to_store="."):
+def py2_read_url(url, dir_to_store='.'):
     import urllib2
-    line = urllib2.urlopen(url + "licfile.txt").read()
+    line = urllib2.urlopen(url + 'licfile.txt').read()
     filename, _ = tuple(line.split(','))
 
     f = urllib2.urlopen(url + filename)
     path = os.path.join(dir_to_store, filename)
-    with open(path, "wb") as licfile:
+    with open(path, 'wb') as licfile:
         licfile.write(f.read())  # lic file is small
     print('downloaded as ' + path)
 
-def py3_read_url(url, dir_to_store="."):
+def py3_read_url(url, dir_to_store='.'):
     import urllib.request
-    line = urllib.request.urlopen(url + "licfile.txt").read()
+    line = urllib.request.urlopen(url + 'licfile.txt').read()
     filename, _ = tuple(line.split(b','))
     filename = filename.decode('ascii')
 
     path = os.path.join(dir_to_store, filename)
     path = os.path.normpath(path)
     f = urllib.request.urlopen(url + filename)
-    with open(path, "wb") as licfile:
+    with open(path, 'wb') as licfile:
         licfile.write(f.read())  # lic file is small
     print('downloaded as ' + path)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import sys
+    dir_to_store = os.path.join(os.environ['userprofile'], 'Desktop')
+    if len(sys.argv) > 1:
+        dir_to_store = sys.argv[1] if os.path.exists(sys.argv[1]) else '.'
+    print('file will store at ' + dir_to_store)
+    # sys.exit(0)
     if sys.version_info.major == 2:
-        py2_read_url(base_url)
+        py2_read_url(base_url, dir_to_store)
     else:
-        py3_read_url(base_url, "d:\\")
+        py3_read_url(base_url, dir_to_store)
