59c817d441f29de265c036fbb5ebf48d5972de91
Huang Jie <jie.huang@aqrose.com>
2020-04-02 10:42:56 +0800

    add script for download halcon license

---
+++ get_halcon_lic/download_lic.py
@@ -0,0 +1,35 @@
+# wrote this script for my friend LT
+import os
+
+base_url = "https://www.51halcon.com/down/license/"
+
+def py2_read_url(url, dir_to_store="."):
+    import urllib2
+    line = urllib2.urlopen(url + "licfile.txt").read()
+    filename, _ = tuple(line.split(','))
+
+    f = urllib2.urlopen(url + filename)
+    path = os.path.join(dir_to_store, filename)
+    with open(path, "wb") as licfile:
+        licfile.write(f.read())  # lic file is small
+    print('downloaded as ' + path)
+
+def py3_read_url(url, dir_to_store="."):
+    import urllib.request
+    line = urllib.request.urlopen(url + "licfile.txt").read()
+    filename, _ = tuple(line.split(b','))
+    filename = filename.decode('ascii')
+
+    path = os.path.join(dir_to_store, filename)
+    path = os.path.normpath(path)
+    f = urllib.request.urlopen(url + filename)
+    with open(path, "wb") as licfile:
+        licfile.write(f.read())  # lic file is small
+    print('downloaded as ' + path)
+
+if __name__ == "__main__":
+    import sys
+    if sys.version_info.major == 2:
+        py2_read_url(base_url)
+    else:
+        py3_read_url(base_url, "d:\\")
